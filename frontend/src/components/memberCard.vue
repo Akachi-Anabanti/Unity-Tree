c4fd32eb2c047664d3d4ebd5fcfeebfcd07cd622
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-05-17 10:14:38 +0100

    updated the member card css and card display

--- frontend/src/components/memberCard.vue
+++ frontend/src/components/memberCard.vue
@@ -11,10 +11,10 @@ const useFamily = useFamilyStore()
 const person = defineProps({
   id: { type: String },
   img: {},
-  first_name: { type: String, required: true },
+  first_name: { type: String},
   last_name: {},
-  date_of_birth: { required: true },
-  role: { type: String, required: true }
+  date_of_birth:{},
+  role: { type: String }
 })
 
 const emit = defineEmits(['removeMember', 'cardClick'])
@@ -28,17 +28,29 @@ const handleCardClick = () => {
 const handleProfileClick = () => {
   router.push({ name: 'profile', params: { userId: person.id } })
 }
+
+const dateFormat = (date) =>{
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+let formattedDate = date.toLocaleDateString("en-US", options);
+return formattedDate
+}
+
+// converts to JS date Object format
+let dob = new Date(person.date_of_birth);
+let formattedDate = dateFormat(dob);
+
+// Capitalize functon
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+// Capitalzes the role funtion
+let role = capitalize(person.role)
+
 </script>
 
 <template>
   <VaCard :bordered="false" @click="handleCardClick">
-    <VaIcon
-      name="delete"
-      size="large"
-      @click.stop="handleRemove"
-      color="danger"
-      v-if="authStore.getCurrentUserId === useFamily.getCreatorId"
-    />
     <div class="card-avatar">
       <VaAvatar
         :src="person.img ? person.img : 'https://randomuser.me/api/portraits/men/1.jpg'"
@@ -46,15 +58,44 @@ const handleProfileClick = () => {
         @click.stop="handleProfileClick"
       />
     </div>
-    <VaCardTitle class="mb-4">{{ person.first_name }} {{ person.last_name }}</VaCardTitle>
-    <VaCardContent>{{ person.date_of_birth }}</VaCardContent>
+    <VaCardTitle class="">
+      {{ person.first_name }} {{ person.last_name }}
+    </VaCardTitle>
+      <VaCardContent>
+        <div><em>{{ role  }}</em></div>
+        <div class="mb-4"><strong>Born: </strong>{{ formattedDate }}</div>
+    </VaCardContent>
+    <VaCardActions align="between" style="margin-top: 10px;">
+      <VaIcon 
+      name="info"
+      size="medium"
+      color="success"
+      @click=""
+      />
+
+      <VaIcon
+      name="edit_square"
+      size="medium"
+      color="primary"
+      @click=""
+      v-if="authStore.getCurrentUserId === useFamily.getCreatorId || person.id === authStore.getCurrentUserId"
+    />
+    <VaIcon
+      name="delete"
+      size="medium"
+      @click.stop="handleRemove"
+      color="danger"
+      v-if="authStore.getCurrentUserId === useFamily.getCreatorId"
+    />
+    
+    </VaCardActions>
+ 
   </VaCard>
 </template>
 
 <style scoped>
 .va-card {
   width: 150px;
-  height: 200px;
   padding: 20px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.15);
   margin-bottom: 20px;
@@ -62,6 +103,7 @@ const handleProfileClick = () => {
 }
 .va-card__content {
   padding: 0;
+  /* background-color: aqua */
 }
 .va-avatar {
   margin: 0.5rem;
@@ -75,13 +117,9 @@ const handleProfileClick = () => {
   justify-content: center;
   align-items: center;
 }
-.va-card-actions {
+
+/* .va-card__actions{
   display: flex;
-  justify-content: space-between;
-}
-.va-icon {
-  position: absolute;
-  right: 0;
-  top: 0;
-}
+  justify-content: space-evenly;
+} */
 </style>
